b4206ed464acb6f6b238f156fe236eea3720a5cb
Ilya <dwilya4@mail.ru>
2022-11-19 13:00:34 +0300

    control

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberFirst = Convert.ToInt32(Console.ReadLine());
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+if (numberFirst > numberSecond)
+{
+    Console.WriteLine($"max = {numberFirst}");
+}
+else
+{
+    Console.WriteLine($"max = {numberSecond}");
+}
---
+++ Task_8/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (number > 0)
+{
+    while (count <= number)
+    {
+        Console.Write(count + " ");
+        count += 2;
+    }
+}
+else
+{
+    Console.WriteLine("число не больше 0");
+}
