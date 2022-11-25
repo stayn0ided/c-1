17dddc1ce0365f8843513184f12b7303dd15efc4
Ilya <dwilya4@mail.ru>
2022-11-25 03:43:44 +0300

    Domashka 2

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1000 && number > 99)
+{
+    int SecondNumber = (number / 10) % 10;
+    Console.WriteLine("Вторая цифра числа - " + SecondNumber);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number < 8)
+{
+    if (number < 6)
+    {
+        Console.WriteLine(number + " -> нет");
+    }
+    else
+    {
+        Console.WriteLine(number + " -> да");
+    }
+}
+else
+{
+    Console.WriteLine("Неподходящее число");
+}
