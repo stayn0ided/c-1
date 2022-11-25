3630a9c68c7a5766e32a139aff2b9d113674fea3
Ilya <dwilya4@mail.ru>
2022-11-25 03:52:32 +0300

    Domashka2final

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,1 +1,15 @@
 ﻿// Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 > 0)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.WriteLine("Третья цифра числа - " + number);
+}
+else
+{
+    Console.WriteLine("В числе меньше трех цифр");
+}
