e7938dba781d435b370ff95236770995d77ffde1
Ilya <dwilya4@mail.ru>
2022-11-28 01:31:00 +0300

    Poryadok na githube

---
+++ Task_04/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberFirst = Convert.ToInt32(Console.ReadLine());
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+int numberThird = Convert.ToInt32(Console.ReadLine());
+int max = numberFirst;
+if (numberSecond > max)
+{
+    max = numberSecond;
+}
+if (numberThird > max)
+{
+    max = numberThird;
+}
+Console.WriteLine(max);
---
+++ Task_06/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
